5f0a5682223395f944a3749ecabde2ccb7de4461
github username <github email address>
2020-01-19 14:16:32 -0500

    frontend

---
+++ src/All_Gear.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Gear_Card from './Gear_Card';
+
+export default class All_Gear extends React.Component{
+    render() {
+        return (
+           <div>
+               "All_Gear"
+               <Gear_Card/>
+              
+           </div>
+        );
+     }
+  }
+
+
---
+++ src/Gear_Container.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import All_Gear from './All_Gear'
+
+export default class Gear_Container extends React.Component{
+    render() {
+fetch('')
+
+        return (
+           <div>
+               "Gear Container"
+              <All_Gear/>
+              
+           </div>
+        );
+     }
+  }
+
+
